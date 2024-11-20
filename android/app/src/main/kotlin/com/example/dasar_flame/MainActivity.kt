11c83bfd00add7968c1acb2f2fb4fa4e4cf9a9fc
indinovia <indinovia.xpplg2@gmail.com>
2024-11-20 13:31:26 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/dasar_flame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dasar_flame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/component/bgParallaxComponent.dart
@@ -0,0 +1,32 @@
+import 'package:dasar_flame/component/ship.dart';
+import 'package:flame/components.dart';
+import 'package:flame/parallax.dart';
+import 'package:flutter/painting.dart';
+
+class Bgparallaxcomponent extends ParallaxComponent with HasGameRef {
+  final _parallelImage = [
+    ParallaxImageData("parallax/bkgd_0.png"),
+    ParallaxImageData("parallax/bkgd_1.png"),
+    ParallaxImageData("parallax/bkgd_2.png"),
+    ParallaxImageData("parallax/bkgd_3.png"),
+    ParallaxImageData("parallax/bkgd_4.png"),
+    ParallaxImageData("parallax/bkgd_5.png"),
+    ParallaxImageData("parallax/bkgd_6.png"),
+  ];
+
+  @override
+  Future<void> onLoad() async {
+    parallax = await game.loadParallax(_parallelImage,
+        baseVelocity: Vector2(0,
+            -10), //jika yang depan -10, maka awan dan lain2 akan bergerak ke samping, jika yang di belakang -10 maka akan bergerak ke bawah
+        velocityMultiplierDelta:
+            Vector2(1, 1), //lebih smooth perubahan dari 1.5 menjadi 1
+        repeat: ImageRepeat.repeat);
+  }
+
+  void changeSpeedBasedShip(Ship player) {
+    if (parallax != null) {
+      parallax!.baseVelocity = player.arah * 10;
+    }
+  }
+}
